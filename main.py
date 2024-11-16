5f5091afe352271ff996f2ba0f6812c0c824551f
Jordan Legg <149483255+404missinglink@users.noreply.github.com>
2024-11-16 21:56:15 +0000

    fixed import

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, JSONResponse
 from llm_service import LLMService, ChatRequest
+from pathlib import Path
+from groq_services import GroqServices
 import os
 import json
 
@@ -11,6 +13,8 @@ llm_service = LLMService()
 # Configuration
 MAX_FILE_SIZE = 25 * 1024 * 1024  # 25MB
 
+parent_directory = os.path.dirname(os.getcwd())
+AUDIO_DIR = f"{parent_directory}/audio-files"
 
 origins = ["http://localhost:3000", "http://127.0.0.1:8000"]
 app.add_middleware(
@@ -61,3 +65,36 @@ async def chat(request: ChatRequest):
             "X-Accel-Buffering": "no"
         }
     )
+
+@app.post("/transcript/")
+async def upload_audio(file: UploadFile = File(...)):
+    SAVE_DIR = Path("uploaded_audio")
+    SAVE_DIR.mkdir(exist_ok=True)
+
+    try:
+        if not file:
+            return JSONResponse(
+                status_code=400,
+                content={"error": "No file uploaded"}
+            )
+
+        file_path = SAVE_DIR / file.filename
+        with file_path.open("wb") as audio_file:
+            audio_file.write(await file.read())
+
+        filename = os.path.dirname(__file__) + f"/{file_path}"
+        transcription = GroqServices.speech_to_text(filename)
+        os.remove(filename)
+
+        return JSONResponse(
+            status_code=200,
+            content={
+                "transcription": transcription,
+            }
+        )
+
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={"error": f"An error occurred: {str(e)}"}
+        )
